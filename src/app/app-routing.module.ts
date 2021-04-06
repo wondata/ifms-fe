d1997a765b30e1698fb2727098d5c7d37a40f889
Abel Adane <abel.adane@hagersew.com>
2021-04-06 10:45:00 +0300

    added dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutDefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'auth',
     pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: LayoutDefaultComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+    ],
   },
 ];
 @NgModule({
