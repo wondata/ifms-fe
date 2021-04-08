a05e2145964bb568dc5ee44ee76c4ea28bf383b2
Abel Adane <abel.adane@hagersew.com>
2021-04-08 10:56:12 +0300

    changed fims to ifms

--- src/app/modules/settings/apis/settings.api.service.ts
+++ src/app/modules/settings/apis/settings.api.service.ts
@@ -12,12 +12,6 @@ export class SettingsApiService {
   contentType = 'Content-Type';
   contentApplication = 'application/json';
 
-  // getAllUsers(query: Query = {}): Observable<Paginated<Employee>> {
-  //   query.size = query.size ?? 10;
-  //   query.sort = query.sort ?? 'id,desc';
-
-  //   return this.http.get<Paginated<Employee>>(userManagementEndpoints.list, { params: (query as unknown) as { [param: string]: string } });
-  // }
   voucherType(): Observable<any> {
     return this.http.get<voucherType>(SettingsEndpoints.voucherType).pipe(
       map((value: any) => {
